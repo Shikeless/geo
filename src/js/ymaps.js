187d337a2b3e400616f448ed10bb5dc684b060aa
Shikeless <Shikeless@gmail.com>
2019-06-21 23:23:19 +0300

    working with pop-up

--- src/js/ymaps.js
+++ src/js/ymaps.js
@@ -1,14 +1,45 @@
 function mapInit() {
   
-  ymaps.ready(() => {
-    console.log("111");
+    ymaps.ready(() => {
 
-    let map = new ymaps.Map("map", {
-      center: [55.73367, 37.587874], // Moscow
-      zoom: 10.2
-    })
-  })
+        let map = new ymaps.Map("map", {
+          center: [55.73367, 37.587874], // Moscow
+          zoom: 10.2
+        })
+
+
+        var myPlacemark;
+
+
+       
+
+
+
+
+
+
+        map.events.add('click', function (e) {
+        	var coords = e.get('coords');
+            myPlacemark = createPlacemark(coords);
+            map.geoObjects.add(myPlacemark);
+            myPlacemark.balloon.open();
+        });
+
+        function createPlacemark(coords) {
+            console.log('placemark');
+            return new ymaps.Placemark(coords, {
+                preset: 'islands#violetDotIconWithCaption',
+                draggable: false
+            }, {balloonLayout: MyBalloonLayout});
+
+        }
+
+
+  });
 }
+
+
+
 
 export {
   mapInit
