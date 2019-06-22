91cf696a0d7462e8e8881d1c14a13909b1238d7a
Shikeless <Shikeless@gmail.com>
2019-06-23 01:11:20 +0300

    work in progress

--- src/index.js
+++ src/index.js
@@ -2,3 +2,81 @@ import { mapInit } from './js/ymaps';
 
 window.onload = mapInit();
 
+		var pa = document.querySelector("#pa");
+		console.log(pa);
+		console.log(map);
+
+		function creating() {
+			var pos = document.createElement('div')
+			pos.id = "pos";
+			pos.classList = "card"
+			console.log(pos)
+			var h4 = document.createElement('h4')
+			h4.id = "header";
+			var container = document.createElement('div')
+			container.classList = "container";
+			var comBox = document.createElement('div');
+			comBox.id = "com-box";
+			comBox.classList = "com-box";
+			var h3 = document.createElement('h3');
+			h3.innerHTML = "ВАШ ОТЗЫВ"
+			var nameInput = document.createElement('input');
+			nameInput.id = "name";
+			nameInput.type = "text";
+			nameInput.name= "name";
+			nameInput.placeholder = "Введите имя";
+			var placeInput = document.createElement('input');
+			placeInput.id = "place";
+			placeInput.type = "text";
+			placeInput.name= "place";
+			placeInput.placeholder = "Введите название места";
+			// var com = document.createElement('textarea');
+			// com.id = "comment";
+			// com.type = "text";
+			// com.name= "comment";
+			// com.placeholder = "Оставьте свой коментарий";
+			console.log(nameInput);
+			var addDiv = document.createElement('div');
+			addDiv.align = "right";
+			addDiv.style = "transform: translateY(-45px);"
+			console.log(addDiv);
+			var but = document.createElement('button');
+			but.id = "addButton";
+			but.classList = "button";
+			but.textContent = "Добавить"
+			pos.appendChild(h4);
+			pos.appendChild(container);
+			container.appendChild(comBox);
+			container.appendChild(h3);
+			container.appendChild(nameInput);
+			container.appendChild(placeInput);
+			// container.appendChild(com);
+			container.appendChild(addDiv);
+			addDiv.appendChild(but);
+			pa.appendChild(pos);
+		}
+		creating();
+
+
+
+
+
+
+
+        // document.addEventListener('click', function(e) {
+                           
+
+        //                 // var source = document.getElementById("entry-template").innerHTML; //определяем шаблон как переменную
+        //                 // var template = Handlebars.compile(source);  //компилируем шаблон
+        //                 // var html = template(data);
+        //                 // const comBox = document.querySelector('#com-box')
+        //                 // comBox.innerHTML = html;
+                
+        //                 document.getElementById('pos').style.left = e.clientX;
+        //                 document.getElementById('pos').style.top = e.clientY;
+        //                 document.getElementById('pos').style.display='block';
+                        
+                    
+        //         });
+
+
--- src/js/ymaps.js
+++ src/js/ymaps.js
@@ -4,38 +4,65 @@ function mapInit() {
 
         let map = new ymaps.Map("map", {
           center: [55.73367, 37.587874], // Moscow
-          zoom: 10.2
+          zoom: 10.2,
+          propagateEvents: true
         })
 
 
-        var myPlacemark;
 
-
-       
-
-
-
-
-
-
+      
         map.events.add('click', function (e) {
+      
+            var myPlacemark;
+            var dat = {name: 'alex', lastName: 'peter'};
+            var dat2 = {name: 'alex', lastName: 'peter', comment: 'aaasdfasdfadf'}
         	var coords = e.get('coords');
-            myPlacemark = createPlacemark(coords);
+            myPlacemark = createPlacemark(coords, dat);
             map.geoObjects.add(myPlacemark);
-            myPlacemark.balloon.open();
+
+            myPlacemark.events.add('click', (e) => {
+            const marker = e.get('target');
+                console.log(marker.properties.get('hintContent'));
+                function change() {
+                    marker.properties
+                        .set({
+                            hintContent: dat2
+                        });
+                    console.log(e.get('target').properties.get('hintContent'))
+                }
+                setTimeout(change, 2000);      
+            })
+
+            // map.geoObjects.events.add('click', e => {
+            //     const marker = e.get('target').properties.get('hintContent');
+            //     console.log(marker);
+            // });
+        document.getElementById('pos').style.left = coords[0];
+        document.getElementById('pos').style.top = coords[1];
+        document.getElementById('pos').style.display='block';
+                        
+                    
+        console.log(coords);
+        console.log(document.getElementById('pos').style)
+        
         });
 
-        function createPlacemark(coords) {
-            console.log('placemark');
-            return new ymaps.Placemark(coords, {
-                preset: 'islands#violetDotIconWithCaption',
-                draggable: false
-            }, {balloonLayout: MyBalloonLayout});
 
+
+        function createPlacemark(coords, data) {
+            return new ymaps.Placemark(coords, {
+                hintContent: data
+            }, {
+                preset: 'islands#violetDotIconWithCaption',
+                draggable: false,
+                openHintOnHover: false
+            });
         }
 
 
-  });
+      
+        
+    });
 }
 
 
