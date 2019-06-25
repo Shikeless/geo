43090a5e33015a80a99ca74dc842e54121d8acb4
Shikeless <Shikeless@gmail.com>
2019-06-25 22:08:02 +0300

    work

--- src/js/ymaps.js
+++ src/js/ymaps.js
@@ -1,4 +1,5 @@
 import render from "../templates/comment.hbs"
+import render1 from "../templates/popup.hbs"
 import { getData } from './date';
 
 var date = getData();
@@ -35,12 +36,16 @@ function mapInit() {
                     obj.coords = coords;
 
                     popup(map, mouseX, mouseY, obj);
-                });   
+                });  
         });
     });
 }
 
 function popup(map ,x, y, obj) {
+    var pop = document.getElementById('pos');
+
+    pop.innerHTML = render1();
+
     var header = document.getElementById('header');
 
     header.innerHTML = obj.adress;
@@ -49,12 +54,14 @@ function popup(map ,x, y, obj) {
 
     document.getElementById('pos').style.top = `${y}px`;
 
-    document.getElementById('pos').style.display='block';
+    document.getElementById('pos').style.display = 'block';
     
-    addComment(map, obj);
+    addComment(map, x, y, obj);
 }
 
-function createPlacemark(map ,obj) {
+
+
+function createPlacemark(map, x, y, obj, comBox) {
     var myPlacemark = new ymaps.Placemark(obj.coords, {
         hintContent: obj.comments
     }, {
@@ -64,14 +71,24 @@ function createPlacemark(map ,obj) {
     });
 
     map.geoObjects.add(myPlacemark);
+
+    myPlacemark.events.add('click', e => {
+        const marker = e.get('target');
+
+        console.log(marker.properties._data.hintContent);
+
+        obj.comments = marker.properties._data.hintContent;
+       
+        popup(map, x, y, obj);
+    });
+
 }
 
-function addComment(map, obj) {
+function addComment(map, x, y, obj) {
     var but = document.querySelector('#addButton');
 
     but.addEventListener('click', e => {
         var comm = {};
-        console.log('click');
 
         var cord = obj.coords;
 
@@ -80,6 +97,8 @@ function addComment(map, obj) {
         var place = document.querySelector('#place');
 
         var comment  = document.querySelector('#comment');
+
+        const comBox = document.querySelector('#com-box');
 
         comm.name = name.value;
 
@@ -97,7 +116,9 @@ function addComment(map, obj) {
 
         obj.comments.list.push(comm);
 
-        createPlacemark(map, obj); 
+        createPlacemark(map, x, y, obj); 
+
+        comBox.innerHTML = render(obj.comments);
     });
 }
 
